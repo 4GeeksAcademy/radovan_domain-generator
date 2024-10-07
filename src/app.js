cc9e91ae11c38dc2f4f46e38b32c6b55874a7bff
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2024-10-07 00:08:22 +0000

    lista <ul> de dominios

--- src/app.js
+++ src/app.js
@@ -13,20 +13,22 @@ window.onload = () => {
 let generarDominio = () => {
   let pronoun = ["the", "your", "our", "my"];
   let adj = ["great", "big", "gorgeous", "exquisite"];
-  let noun = ["jogger", "racoon", "ornitorrinco", "dog", "cat"];
-  let ext = [".com.ve"];
+  let noun = ["pata", "racoon", "ornitorrinco", "dog", "cat"];
+  let ext = [".com.ve", ".net", ".us", ".io"];
 
   let listaHTML = "<ul>";
 
   for (let p = 0; p < pronoun.length; p++) {
     for (let a = 0; a < adj.length; a++) {
       for (let n = 0; n < noun.length; n++) {
-        let elem1 = pronoun[p];
-        let elem2 = adj[a];
-        let elem3 = noun[n];
-        let elem4 = ext;
-        let genNom = elem1 + elem2 + elem3 + elem4;
-        listaHTML += "<li>" + genNom + "</li>";
+        for (let e = 0; e < ext.length; e++) {
+          let elem1 = pronoun[p];
+          let elem2 = adj[a];
+          let elem3 = noun[n];
+          let elem4 = ext[e];
+          let genNom = elem1 + elem2 + elem3 + elem4;
+          listaHTML += "<li>" + genNom + "</li>";
+        }
       }
     }
   }
